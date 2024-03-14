a066316cada3527827a290725d4b7a3d56356445
DavidSimplon <8061278-DavidSimplon@users.noreply.gitlab.com>
2024-03-14 09:08:13 +0100

    Initial commit

---
+++ src/changestyle.js
@@ -0,0 +1,4 @@
+let jsword = document.getElementById('jsmot')
+jsword.style.color = "black"
+jsword.style.fontSize = 'medium'
+jsword.style.backgroundColor = "yellow"
---
+++ src/components/StyleChange.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="handleClick" class="message" id="vue" >Clique ici</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+        let vueword = document.getElementById('vue')
+        vueword.textContent='VueJs'
+        vueword.style.color = 'black'
+        vueword.style.backgroundColor = 'aqua';
+    },
+  },
+};
+</script>
+
+<style>
+.message{
+    color: yellow;
+    background-color: coral;
+    font-weight: bold;
+}
+</style>
